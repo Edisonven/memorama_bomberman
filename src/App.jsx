b569d76336367e9d03794798f6ebcc7cd5f5a3f8
Edison <aalevenegass@gmail.com>
2024-03-19 12:47:24 -0300

    Asignanod imágen de fondo y ajustando posición de cartas

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,6 @@ function App() {
 
   return (
     <section className="app__container">
-      <h1 className="app__title">Memorama</h1>
       <div className="app__body">
         {cards.map((card, index) => {
           return (
