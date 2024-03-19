a7d0d126df2f60ae706546734ea84f0067827835
Edison <aalevenegass@gmail.com>
2024-03-19 00:58:48 -0300

    Creando función para cambiar estado de cardFlipped

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,11 +5,20 @@ import ReactCardFlip from "react-card-flip";
 const Card = ({ name, number, frontFace }) => {
   const [isFlipped, setIsFlipped] = useState(false);
 
+  const handleClickCard = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
     <div className="card__container">
       <ReactCardFlip isFlipped={isFlipped}>
+        <img
+          className="card__img"
+          src="./2.png"
+          alt=""
+          onClick={handleClickCard}
+        />
         <img className="card__img" src={frontFace} alt="" />
-        <img src="" alt="" />
       </ReactCardFlip>
     </div>
   );
