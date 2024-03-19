d81dad88836ba26233e18e866bf7c0fa8b3bc248
Edison <aalevenegass@gmail.com>
2024-03-19 00:16:14 -0300

    Creando js con imágenes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ import "../Card/card.css";
 const Card = () => {
   return (
     <div className="card__container">
-      <h1>Hola soy una card</h1>
+
     </div>
   );
 };
---
+++ src/images.js
@@ -0,0 +1,76 @@
+import src1 from "../public/enemy_1.png";
+import src2 from "../public/enemy_2.png";
+import src3 from "../public/enemy_3.png";
+import src4 from "../public/enemy_4.png";
+import src5 from "../public/enemy_5.png";
+import src6 from "../public/enemy_6.png";
+import src7 from "../public/enemy_7.png";
+import src8 from "../public/enemy_8.png";
+import src9 from "../public/bomb.png";
+import src10 from "../public/power_1.png";
+import src11 from "../public/character.png";
+import src12 from "../public/door.png";
+
+const images = [
+  {
+    id: 1,
+    src: src1,
+    name: "enemy_1",
+  },
+  {
+    id: 2,
+    src: src2,
+    name: "enemy_2",
+  },
+  {
+    id: 3,
+    src: src3,
+    name: "enemy_3",
+  },
+  {
+    id: 4,
+    src: src4,
+    name: "enemy_4",
+  },
+  {
+    id: 5,
+    src: src5,
+    name: "enemy_5",
+  },
+  {
+    id: 6,
+    src: src6,
+    name: "enemy_6",
+  },
+  {
+    id: 7,
+    src: src7,
+    name: "enemy_7",
+  },
+  {
+    id: 8,
+    src: src8,
+    name: "enemy_8",
+  },
+  {
+    id: 9,
+    src: src9,
+    name: "bomb",
+  },
+  {
+    id: 10,
+    src: src10,
+    name: "power_1",
+  },
+  {
+    id: 11,
+    src: src11,
+    name: "character",
+  },
+  {
+    id: 12,
+    src: src12,
+    name: "door",
+  },
+];
+export { images };
