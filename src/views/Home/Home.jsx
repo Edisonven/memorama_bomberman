2fb440c69b8e3d04a672ca8bf4f20b1c8774e702
Edison <aalevenegass@gmail.com>
2024-03-19 21:21:40 -0300

    Ajustando imágen de logo con aspect/ratio

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <section className="home__container">
       <div className="home__logo__container">
-        <img className="home__logo__img" src="/logo.jpg" alt="" />
+        <img className="home__logo__img" src="/logo.webp" alt="" />
       </div>
       <div className="home__stargame">
         <div className="home__startgame__link__container">
